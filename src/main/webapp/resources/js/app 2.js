d6acb625180596a64c23882a719f5e9cd9c9b635
Gregory <gregory_lammers@hotmail.com>
2018-03-14 11:20:34 +0100

    fix kweetservicetest

---
+++ src/main/webapp/resources/js/app 2.js
@@ -0,0 +1,52 @@
+let stompClient = null
+
+
+function connect() {
+    let socket = new SockJS('/com-s63d-d')
+    stompClient = Stomp.over(socket)
+    stompClient.debug = () => {}
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame)
+        stompClient.subscribe('/location', function (coord) {
+            showCoord(JSON.parse(coord.body))
+        })
+    })
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect()
+    }
+    console.log("Disconnected")
+}
+
+
+
+let markers = []
+let updates = []
+function showCoord(locationUpdate) {
+    let id = locationUpdate.id
+    let coords = locationUpdate.coords
+    let marker = markers[id]
+    if (marker == null) {
+        marker = L.marker(coords).addTo(map)
+        marker.bindTooltip("Car <b>" + id + "</b>")
+        markers[id] = marker
+        updates[id] = 0
+    } else {
+        marker.setLatLng(coords).update()
+        updates[id]++
+    }
+}
+
+let map
+$(function () {
+    map = L.map('mapid').setView([47.516231, 14.550072], :sunglasses:
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: 'pk.eyJ1IjoibXVyZnp3IiwiYSI6ImNqZHluaDUyMDFkcDQycXM0cHRuc2VsbWgifQ.j8GNzXpH9zA-eAMa36h9cg'
+        }).addTo(map)
+    connect()
+})
--- src/test/java/service/KweetServiceTest.java
+++ src/test/java/service/KweetServiceTest.java
@@ -27,7 +27,7 @@ public class KweetServiceTest {
         Kweet kweet = new Kweet("dit is test kweet", user);
         kweetService.addKweet(kweet);
 
-        Mockito.verify(kweetDAO, Mockito.times(1)).createKweet(kweet);
+        Mockito.verify(kweetDAO, Mockito.times(1)).addKweet(kweet);
     }
 
     @Test
